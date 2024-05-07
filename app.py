61941a5cc9774cc27bce409f6a1c0a108c24e271
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 17:27:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,37 +11,25 @@ data = pd.read_csv(url)
 st.write(data.head(10))
 
 # Scatter plot with day against tip
-plt.scatter(data['day'], data['tip'])
+fig1, ax1 = plt.subplots()
+ax1.scatter(data['day'], data['tip'])
+ax1.set_title("Scatter Plot 1")
+ax1.set_xlabel('Day')
+ax1.set_ylabel('Tip')
+st.pyplot(fig1)
 
-# Adding Title to the Plot
-plt.title("Scatter Plot")
+# Scatter plot with day against size
+fig2, ax2 = plt.subplots()
+ax2.scatter(data['day'], data['size'])
+ax2.set_title("Scatter Plot 2")
+ax2.set_xlabel('Day')
+ax2.set_ylabel('Size')
+st.pyplot(fig2)
 
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-# Scatter plot with day against tip
-fig, ax = plt.subplots()
-scatter = ax.scatter(data['day'], data['tip'], c=data['size'], s=data['total_bill'])
-
-# Adding Title to the Plot
-plt.title("Scatter Plot")
-
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-plt.colorbar(scatter)
-
-# Scatter plot with day against tip
-plt.plot(data['tip'])
-plt.plot(data['size'])
-
-# Adding Title to the Plot
-plt.title("Scatter Plot")
-
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-st.pyplot(fig)
+# Scatter plot with total_bill against tip
+fig3, ax3 = plt.subplots()
+ax3.scatter(data['total_bill'], data['tip'])
+ax3.set_title("Scatter Plot 3")
+ax3.set_xlabel('Total Bill')
+ax3.set_ylabel('Tip')
+st.pyplot(fig3)
