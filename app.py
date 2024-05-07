868ddd0c34a6ee7c00e4bc02feaf003f295daa4a
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 15:39:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+url = "https://raw.githubusercontent.com/afridalailiyah/repository/master/tips.csv"
+
 # reading the database
-data = pd.read_csv("tips.csv")
+data = pd.read_csv("url")
 
 # printing the top 10 rows
 display(data.head(10))
