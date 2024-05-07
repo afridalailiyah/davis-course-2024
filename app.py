9dc11333dabbf27be537b79bcfe6b7c220529144
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 17:20:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Menambahkan judul dengan st.title()
-st.title("💻 Menyajikan data tips.csv pada platform Streamlit!")
 
 url = "https://raw.githubusercontent.com/afridalailiyah/davis-course-2024/master/tips.csv"
 
@@ -36,55 +32,5 @@ plt.xlabel('Day')
 plt.ylabel('Tip')
 
 plt.colorbar(scatter)
-
-# Bar chart with day against tip
-plt.bar(data['day'], data['tip'])
-
-plt.title("Bar Chart")
-
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-# histogram of total_bills
-plt.hist(data['total_bill'])
-
-plt.title("Histogram")
-
-# Set mode.use_inf_as_null to True
-with pd.option_context('mode.use_inf_as_null', True):
-    # draw lineplot
-    sns.lineplot(x="sex", y="total_bill", data=data)
-
-# setting the title using Matplotlib
-plt.title('Title using Matplotlib Function')
-
-sns.scatterplot(x='day', y='tip', data=data,)
-
-# plotting the scatter chart
-fig = px.scatter(data, x="day", y="tip", color='sex')
-
-# plotting the scatter chart
-fig = px.line(data, y='tip', color='sex')
-
-plot = px.Figure(data=[px.Scatter(
-    y=data['tip'],
-    mode='lines',)
-])
-
-plot.update_layout(
-    xaxis=dict(
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1,
-                    step="day",
-                    stepmode="backward"),
-            ])
-        ),
-        rangeslider=dict(
-            visible=True
-        ),
-    )
-)
 
 st.pyplot(fig)
