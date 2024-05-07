5ef7606245ecf3ad5834c0fabd8e3d035663612b
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 17:29:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ data = pd.read_csv(url)
 # printing the top 10 rows
 st.write(data.head(10))
 
-# Scatter plot with day against tip
+# Scatter plot with day against tip (plot pertama)
 fig1, ax1 = plt.subplots()
 ax1.scatter(data['day'], data['tip'])
 ax1.set_title("Scatter Plot 1")
@@ -18,18 +18,21 @@ ax1.set_xlabel('Day')
 ax1.set_ylabel('Tip')
 st.pyplot(fig1)
 
-# Scatter plot with day against size
+# Scatter plot with day against tip (plot kedua)
 fig2, ax2 = plt.subplots()
-ax2.scatter(data['day'], data['size'])
+scatter = ax2.scatter(data['day'], data['tip'], c=data['size'], s=data['total_bill'])
 ax2.set_title("Scatter Plot 2")
 ax2.set_xlabel('Day')
-ax2.set_ylabel('Size')
+ax2.set_ylabel('Tip')
+plt.colorbar(scatter)
 st.pyplot(fig2)
 
-# Scatter plot with total_bill against tip
+# Scatter plot with tip and size (plot ketiga)
 fig3, ax3 = plt.subplots()
-ax3.scatter(data['total_bill'], data['tip'])
+ax3.plot(data['tip'], label='Tip')
+ax3.plot(data['size'], label='Size')
 ax3.set_title("Scatter Plot 3")
-ax3.set_xlabel('Total Bill')
-ax3.set_ylabel('Tip')
+ax3.set_xlabel('Day')
+ax3.set_ylabel('Tip/Size')
+ax3.legend()
 st.pyplot(fig3)
