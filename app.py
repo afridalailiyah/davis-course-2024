926bf760c7998351f1b6bee04eae0e59018a7c87
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 17:35:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 url = "https://raw.githubusercontent.com/afridalailiyah/davis-course-2024/master/tips.csv"
 
