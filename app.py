2e1fbe9d2da4b4989053f39ee36a4fa5b3e78707
Afrida <126935228+afridalailiyah@users.noreply.github.com>
2024-05-07 15:41:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/afridalailiyah/repository/master/tips.csv"
+url = "https://raw.githubusercontent.com/afridalailiyah/davis-course-2024/master/tips.csv"
 
 # reading the database
 data = pd.read_csv("url")
